ce151e9f2d9a32b6de33ece14662d96edad8b4e8
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-21 14:18:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   def self.find_or_create_by_name (name)
     song = self.find_by_name(name)
     if song == false
-      self.create_by_name(name)
+      song = self.create_by_name(name)
     else
       song
     end
